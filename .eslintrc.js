1a7569f1ea9248d5ca8df2c4eec4630b71012392
Slava Sidorov <sidorov-vv3@mail.ru>
2024-07-10 15:54:22 +0700

    increased the number of characters per line for ESLint to 100. Added the BundleAnalyzerPlugin plugin for bundle analysis. Created and connected errorbinary to catch an error in the application. I created a special button to trigger an error in the application. modified translations.

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,7 +48,7 @@ module.exports = {
             process.env.NODE_ENV === 'prod' ? 'unix' : 'windows',
         ],
         'i18next/no-literal-string': ['error', { markupOnly: true }],
-        'max-len': ['error', { ignoreComments: true }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
