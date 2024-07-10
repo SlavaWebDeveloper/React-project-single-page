0958bd52381ebed85986fae22ba5712ca7f4ffef
Slava Sidorov <sidorov-vv3@mail.ru>
2024-07-11 03:09:20 +0700

    fix

--- src/shared/ui/Pageloader/PageLoader.tsx
+++ src/shared/ui/Pageloader/PageLoader.tsx
@@ -4,7 +4,7 @@ import cls from './PageLoader.module.scss';
 
 interface PageLoaderProps {
     className?: string;
-}
+} 
 
 export const PageLoader = ({ className }: PageLoaderProps) => (
     <div className={classNames(cls.PageLoader, {}, [className])}>
