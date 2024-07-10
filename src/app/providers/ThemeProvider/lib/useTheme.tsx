1107cd0b00449efb9eda84ba5d3ecb0aa42fc922
Slava Sidorov <sidorov-vv3@mail.ru>
2024-07-10 21:27:00 +0700

    add storybook

--- src/app/providers/ThemeProvider/lib/useTheme.tsx
+++ src/app/providers/ThemeProvider/lib/useTheme.tsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react';
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContent } from './ThemeContext';
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
 
-interface useThemeResult {
-  theme: Theme
-  toggleTheme: () => void
+interface UseThemeResult {
+    toggleTheme: () => void;
+    theme: Theme;
 }
 
-export function useTheme(): useThemeResult {
-    const { theme, setTheme } = useContext(ThemeContent);
+export function useTheme(): UseThemeResult {
+    const { theme, setTheme } = useContext(ThemeContext);
 
     const toggleTheme = () => {
         const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
@@ -16,6 +16,7 @@ export function useTheme(): useThemeResult {
     };
 
     return {
-        theme, toggleTheme,
+        theme,
+        toggleTheme,
     };
 }
