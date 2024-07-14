8d87cf4674f537d9f0e890f84da0018712bde639
Slava Sidorov <sidorov-vv3@mail.ru>
2024-07-14 21:07:51 +0700

    Updated ESLint config, added react-hooks rules, and made changes to various components including App, ThemeProvider, Button, and Navbar, also updated styles and translations.

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import Modal from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useCallback, useState } from 'react';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -9,12 +11,25 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                onClick={onToggleModal}
+                className={cls.links}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line */}
+                {t(' Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quo modi, qui sunt quam tenetur, sed doloremque vitae officia corporis nihil impedit.Modi illo debitis odio magni sit dolores sunt.')}
+            </Modal>
         </div>
     );
 };
