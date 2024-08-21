bf901ddecb63dff9869107a7b886fc8b8211f5fb
Slava Sidorov <sidorov-vv3@mail.ru>
2024-08-21 23:00:08 +0700

    Refactor StoreProvider, add dynamic reducer management, and update related components. add Async LoginForm

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,12 +1,14 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import Modal from 'shared/ui/Modal/Modal';
+import { Suspense } from 'react';
+import { Loader } from 'shared/ui/Loader/Loader';
 import cls from './LoginModal.module.scss';
-import { LoginForm } from '../LoginForm/LoginForm';
+import { LoginFormAsync } from '../LoginForm/LoginForm.async';
 
 interface LoginModalProps {
-  className?: string;
-  isOpen: boolean;
-  onClose: () => void
+    className?: string;
+    isOpen: boolean;
+    onClose: () => void
 }
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
@@ -18,6 +20,8 @@ export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
         onClose={onClose}
         lazy
     >
-        <LoginForm />
+        <Suspense fallback={<Loader />}>
+            <LoginFormAsync />
+        </Suspense>
     </Modal>
 );
