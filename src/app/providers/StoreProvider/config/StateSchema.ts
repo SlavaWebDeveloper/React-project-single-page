09789f75778710672f544c007111d501eb769dc7
Slava Sidorov <sidorov-vv3@mail.ru>
2024-08-23 18:24:33 +0700

    Add profile, optimization render

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -2,6 +2,7 @@ import {
     AnyAction, CombinedState, EnhancedStore, Reducer, ReducersMapObject,
 } from '@reduxjs/toolkit';
 import { CounterSchema } from 'entitie/Counter';
+import { ProfileSchema } from 'entitie/Profile';
 import { UserSchema } from 'entitie/User';
 import { LoginSchema } from 'features/AuthByUsername';
 
@@ -9,6 +10,7 @@ export interface StateSchema {
 	counter: CounterSchema;
 	user: UserSchema;
 	loginForm?: LoginSchema;
+	profile?: ProfileSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
