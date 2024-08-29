dc4b9d16918c658202b4631eeadc356dc0b11705
Slava Sidorov <sidorov-vv3@mail.ru>
2024-08-29 23:46:55 +0700

    Updated various files in the app, including StoreProvider, AppRouter, Profile, routeConfig, Button, Input, Text, and Sidebar, with changes to interfaces, components, and functionality.

--- src/entitie/Profile/model/types/profile.ts
+++ src/entitie/Profile/model/types/profile.ts
@@ -1,18 +1,20 @@
-import { Country, Currency } from 'shared/const/common';
+import { Currency } from 'entitie/Currency/model/types/currency';
+import { Country } from 'entitie/Country/model/types/country';
 
 export interface Profile {
-	first: string;
-	lastname: string;
-	age: 24;
-	currency: Currency;
-	country: Country;
-	city: string;
-	username: string;
-	avatar: string;
+	first?: string;
+	lastname?: string;
+	age?: number;
+	currency?: Currency;
+	country?: Country;
+	city?: string;
+	username?: string;
+	avatar?: string;
 }
 
 export interface ProfileSchema {
 	data?: Profile;
+	form?: Profile;
 	isLoading: boolean;
 	error?: string;
 	readonly: boolean;
