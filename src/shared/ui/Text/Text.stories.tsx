6d70b62de2c913cdb38e053fadd2c6e88a988f8c
Slava Sidorov <sidorov-vv3@mail.ru>
2024-12-05 12:42:05 +0700

    add semantics

--- src/shared/ui/Text/Text.stories.tsx
+++ src/shared/ui/Text/Text.stories.tsx
@@ -76,3 +76,10 @@ SizeMDefault.args = {
     text: 'Description Description Description Description',
     size: TextSize.M,
 };
+
+export const SizeS = Template.bind({});
+SizeS.args = {
+    title: 'Title lorem ipsun',
+    text: 'Description Description Description Description',
+    size: TextSize.S,
+};
