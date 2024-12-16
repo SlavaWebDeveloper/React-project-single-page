ed9c228cbd206f99b0ac2c99d8c26362e9a42ef6
Slava Sidorov <sidorov-vv3@mail.ru>
2024-12-16 15:17:45 +0700

    add profile card tests, add user event, add conditional virtualized

--- src/entitie/Article/ui/ArticleList/ArticleList.tsx
+++ src/entitie/Article/ui/ArticleList/ArticleList.tsx
@@ -17,6 +17,7 @@ interface ArticleListProps {
     isLoading?: boolean;
     target?: HTMLAttributeAnchorTarget;
     view?: ArticleView;
+    virtualized?: boolean;
 }
 
 const getSkeletons = (view: ArticleView) => new Array(view === ArticleView.SMALL ? 9 : 3)
@@ -33,6 +34,7 @@ export const ArticleList = memo((props: ArticleListProps) => {
         view = ArticleView.SMALL,
         isLoading,
         target,
+        virtualized = true,
     } = props;
     const { t } = useTranslation();
 
@@ -90,22 +92,37 @@ export const ArticleList = memo((props: ArticleListProps) => {
                 onChildScroll,
                 isScrolling,
                 scrollTop,
+
             }) => (
                 <div
                     ref={registerChild}
                     className={classNames(cls.ArticleList, {}, [className, cls[view]])}
                 >
-                    <List
-                        height={height ?? 700}
-                        rowCount={rowCount}
-                        rowHeight={isBig ? 700 : 330}
-                        rowRenderer={rowRender}
-                        width={width ? width - 80 : 700}
-                        autoHeight
-                        onScroll={onChildScroll}
-                        isScrolling={isScrolling}
-                        scrollTop={scrollTop}
-                    />
+                    {virtualized
+                        ? (
+                            <List
+                                height={height ?? 700}
+                                rowCount={rowCount}
+                                rowHeight={isBig ? 700 : 330}
+                                rowRenderer={rowRender}
+                                width={width ? width - 80 : 700}
+                                autoHeight
+                                onScroll={onChildScroll}
+                                isScrolling={isScrolling}
+                                scrollTop={scrollTop}
+                            />
+                        )
+                        : (
+                            articles.map((item) => (
+                                <ArticleListItem
+                                    article={item}
+                                    view={view}
+                                    key={item.id}
+                                    className={cls.card}
+                                    target={target}
+                                />
+                            ))
+                        )}
                     {isLoading && getSkeletons(view)}
                 </div>
             )}
---
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
@@ -0,0 +1,91 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { Profile } from 'entitie/Profile';
+import { Currency } from 'entitie/Currency';
+import { Country } from 'entitie/Country';
+import userEvent from '@testing-library/user-event';
+import { $api } from 'shared/api/api';
+import { profileReducer } from '../../model/slice/profileSlice';
+import { EditableProfileCard } from './EditableProfileCard';
+
+const profile: Profile = {
+    id: '1',
+    first: 'admin',
+    lastname: 'admin',
+    age: 32,
+    currency: Currency.USD,
+    country: Country.Russia,
+    city: 'Moscow',
+    username: 'admin123',
+};
+
+const options = {
+    initialState: {
+        profile: {
+            readonly: true,
+            data: profile,
+            form: profile,
+        },
+        user: {
+            authData: {
+                id: '1',
+                username: 'admin123',
+            },
+        },
+    },
+    asyncReducers: {
+        profile: profileReducer,
+    },
+};
+
+describe('features/EditableProfileCard', () => {
+    test('read only update to edit', async () => {
+        componentRender(<EditableProfileCard id="1" />, options);
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+        expect(screen.getByTestId('EditableProfileCardHeader.CancelButton')).toBeInTheDocument();
+    });
+
+    test('use edit update to read and update to initial state', async () => {
+        componentRender(<EditableProfileCard id="1" />, options);
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+
+        await userEvent.clear(screen.getByTestId('ProfileCard.firstname'));
+        await userEvent.clear(screen.getByTestId('ProfileCard.lastname'));
+
+        await userEvent.type(screen.getByTestId('ProfileCard.firstname'), 'user');
+        await userEvent.type(screen.getByTestId('ProfileCard.lastname'), 'user1');
+
+        expect(screen.getByTestId('ProfileCard.firstname')).toHaveValue('user');
+        expect(screen.getByTestId('ProfileCard.lastname')).toHaveValue('user1');
+
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.CancelButton'));
+
+        expect(screen.getByTestId('ProfileCard.firstname')).toHaveValue('admin');
+        expect(screen.getByTestId('ProfileCard.lastname')).toHaveValue('admin');
+    });
+
+    test('error data', async () => {
+        componentRender(<EditableProfileCard id="1" />, options);
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+
+        await userEvent.clear(screen.getByTestId('ProfileCard.firstname'));
+
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.SaveButton'));
+
+        expect(screen.getByTestId('EditableProfileCard.Error.Paragraph')).toBeInTheDocument();
+    });
+
+    test('not error data and use PUT request to server', async () => {
+        const mockPutReq = jest.spyOn($api, 'put');
+
+        componentRender(<EditableProfileCard id="1" />, options);
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+
+        await userEvent.clear(screen.getByTestId('ProfileCard.firstname'));
+        await userEvent.type(screen.getByTestId('ProfileCard.firstname'), 'user');
+
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.SaveButton'));
+
+        expect(mockPutReq).toHaveBeenCalled();
+    });
+});
