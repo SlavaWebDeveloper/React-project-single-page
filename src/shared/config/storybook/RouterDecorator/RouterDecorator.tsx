f1ed191ab9c7cf2b79d2bf7b98c6dc7211037c8f
Slava Sidorov <sidorov-vv3@mail.ru>
2024-12-17 13:29:42 +0700

    migrate to react 18, add mock for stotybook

--- src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
@@ -1,9 +1,8 @@
 import { Story } from '@storybook/react';
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
 
-export const RouterDecorator = (story: () => Story) => (
+export const RouterDecorator = (StoryComponent: Story) => (
     <BrowserRouter>
-        {story()}
+        <StoryComponent />
     </BrowserRouter>
 );
